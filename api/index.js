d28d0f44ee4a578873cd506adc7ad85c206cc3da
ShehzorMemon <Shezymemon0180@gmail.com>
2024-06-04 23:02:03 +0500

    Added code to deploy on vercel

--- api/index.js
+++ api/index.js
@@ -29,11 +29,13 @@ app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listings", listingRouter)
 
-app.use(express.static(path.join(__dirname, '/client', 'dist', 'index.html')))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client'))
-})
+app.use(cors(
+    {
+        origin: ["https://realestate-backend-five.vercel.app/"],
+        methods: ['POST', "GET"],
+        credentials: true
+    }
+))
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
